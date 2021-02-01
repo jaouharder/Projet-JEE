df2f530857106f52fff5c57544aea4e9b474466f
Xeno <hpcode@example.com>
2021-01-31 23:56:28 -0100

    Connection with database established ; created object JDBCONFig that will  help us to make query to interact with database tables

--- ReservationSystem/src/main/java/com/ReservationSystem/ReservationSystemApplication.java
+++ ReservationSystem/src/main/java/com/ReservationSystem/ReservationSystemApplication.java
@@ -1,13 +1,36 @@
 package com.ReservationSystem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ReservationSystem.configdb.JDBCONFIG;
 
 @SpringBootApplication
 public class ReservationSystemApplication {
 
+	
 	public static void main(String[] args) {
-		SpringApplication.run(ReservationSystemApplication.class, args);
+		SpringApplication.run(ReservationSystemApplication.class, args);	
+		try {
+			Connection conx=JDBCONFIG.GetConx();
+			Statement stat=conx.createStatement();
+			ResultSet result= stat.executeQuery("select * from agence;");
+			while(result.next()) {
+				 String address=result.getString("agence_name");
+				 String localisation=result.getString("agence_name");
+				 int  disponibilite=result.getInt("disponibilite");
+				 System.out.println("addresse:"+address+",localisation :"+localisation+",disponobilte % :"+disponibilite);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }

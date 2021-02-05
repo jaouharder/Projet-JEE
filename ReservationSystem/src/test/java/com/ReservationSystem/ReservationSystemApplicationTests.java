d4340314178287d4d0f3ec1ab4d3497ba1642e83
MohammedOuttaleb <mohammedouttaleb245@gmail.com>
2021-02-05 04:34:29 -0100

    agencydao class test

--- ReservationSystem/src/test/java/com/ReservationSystem/ReservationSystemApplicationTests.java
+++ ReservationSystem/src/test/java/com/ReservationSystem/ReservationSystemApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ReservationSystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ReservationSystem.dao.AgenceDAO;
+import com.ReservationSystem.model.Agence;
 
 @SpringBootTest
 class ReservationSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		// test of agency methods
+		AgenceDAO agenceService=new AgenceDAO();
+		agenceService.createAgency(new Agence("CIH Paris","Champs Elysee",30));
+		System.out.println("agence with id 1 : "+agenceService.findById(1));
+		List<Agence> list=agenceService.findbyAvailability();
+		System.out.println("agence with order: "+list);
+		
 	}
 
 }

dfc4effc15a962508392262157924ac6e533ea78
EM-Idriss <74628794+EM-Idriss@users.noreply.github.com>
2021-02-06 23:30:55 +0100

    Added Reservation & ReservationDAO

---
+++ ReservationSystem/src/main/java/com/ReservationSystem/dao/ReservationDAO.java
@@ -0,0 +1,164 @@
+package com.ReservationSystem.dao;
+
+import com.ReservationSystem.configdb.JDBCONFIG;
+import com.ReservationSystem.model.Client;
+import com.ReservationSystem.model.Reservation;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationDAO {
+
+    Connection connection = JDBCONFIG.GetConx();
+    Statement statement;
+    ResultSet resultSet;
+    String query;
+
+    public List<Reservation> findAll() {
+
+        try {
+            statement = connection.createStatement();
+            List<Reservation> reservations= new ArrayList<Reservation>();
+            query = "select * from reservation";
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+
+                int reservationId = resultSet.getInt("reservation_id");
+                Time horaire = resultSet.getTime("horaire");
+                int duree = resultSet.getInt("duree");
+
+                //TODO Adapt to class Bureau
+                Bureau bureau = Bureau.findById(resultSet.getString("bureau_id"));
+
+                //TODO Choose the correct method between the 2 following:
+
+                //Method #1
+                //Client client = Client.findById(resultSet.getString("cin_client"));
+
+                //Method #2
+                //type of cin is int in class Client, should be String for example AB12345
+                int cin = resultSet.getInt("cin_client");
+                String prenom = resultSet.getString("prenom_client");
+                String nom = resultSet.getString("nom_client");
+
+                String localisation = "temporary... See TODO";  //TODO Find solution for: the table reservation doesn't contain "localisation" to create Client
+                //possible solution is get "localisation" from Bureau
+
+                String email = resultSet.getString("email_client");
+                Client client = new Client(cin, nom, prenom, localisation, email);
+
+                Reservation reservation = new Reservation(reservationId, horaire, bureau, client, duree);
+                reservations.add(reservation);
+            }
+            return reservations;
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    //TODO Check if these methods need to be static
+
+    public Reservation findById(int reservationId) {
+        try {
+            statement = connection.createStatement();
+            query = "select * from reservation " +
+                    "where reservation_id = " + reservationId + ";";
+            resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                Time horaire = resultSet.getTime("horaire");
+                int duree = resultSet.getInt("duree");
+
+                //TODO Adapt to class Bureau
+                Bureau bureau = BureauDAO.findById(resultSet.getString("bureau_id"));
+
+                //TODO Choose the correct method between the 2 following:
+
+                //Method #1
+                //Client client = Client.findById(resultSet.getString("cin_client"));
+
+                //Method #2
+                //type of cin is int in class Client, should be Strind for example AB12345
+                int cin = resultSet.getInt("cin_client");
+                String prenom = resultSet.getString("prenom_client");
+                String nom = resultSet.getString("nom_client");
+
+                String localisation = "temporary... See TODO";  //TODO Find solution for: the table reservation doesn't contain "localisation" to create Client
+                //possible solution is get "localisation from Bureau
+
+                String email = resultSet.getString("email_client");
+                Client client = new Client(cin, nom, prenom, localisation, email);
+
+                return new Reservation(reservationId, horaire, bureau, client, duree);
+            }
+            else return null;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    public void createReservation(Reservation reservation) {
+        try {
+            statement = connection.createStatement();
+
+            //int reservationId = reservation.getReservationId();
+            Time horaire = reservation.getHoraire();
+            int duree = reservation.getDuree();
+            Client client = reservation.getClient();
+            int bureauId = reservation.getBureau().getBureauId; //TODO Adapt to class Bureau
+
+            //TODO change cin to work as a String if changed in class Client
+            query ="INSERT INTO reservation(cin_client, nom_client, prenom_client, email_client, horaire, duree, bureau_id) " +
+                    "VALUES(" + client.getCin() + ", '" + client.getNom() + "', '" + client.getPrenom() +
+                    "', '" + client.getEmail() + "', '" + horaire + "', " + duree + ", " + bureauId + ");";
+
+            resultSet = statement.executeQuery(query);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    //Updates "horaire" & "bureau_id"
+    public void updateReservation(int reservationId, Time horaire, int bureauId) {
+        try {
+            statement = connection.createStatement();
+
+            query = "UPDATE RESERVATION " +
+                    "SET HORAIRE = '" + horaire + "', " +
+                    "BUREAU_ID = " + bureauId +
+                    " WHERE RESERVATION_ID = " + reservationId + ";";
+
+            statement.executeUpdate(query);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void deleteReservation(int reservationId) {
+        try{
+            statement = connection.createStatement();
+
+            query = "DELETE FROM RESERVATION " +
+                    "WHERE RESERVATION_ID = " + reservationId + ";";
+
+            statement.executeUpdate(query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    public void sendEmailVerification(Reservation reservation) {
+        //TODO Setup email
+        return;
+    }
+
+
+}
---
+++ ReservationSystem/src/main/java/com/ReservationSystem/model/Reservation.java
@@ -0,0 +1,90 @@
+package com.ReservationSystem.model;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.Instant;
+
+public class Reservation {
+
+    private int reservationId;
+    @NotNull
+    private Time horaire;
+    @NotNull
+    private Bureau bureau;
+    @NotNull
+    private Client client;
+    private int duree;
+
+    //TODO Check whether reservationId should be removed or not
+    public Reservation(int reservationId, @NotNull Time horaire, @NotNull Bureau bureau, @NotNull Client client) {
+        this.reservationId = reservationId;
+        this.horaire = horaire;
+        this.bureau = bureau;
+        this.client = client;
+        this.duree = 0;
+    }
+    //this constructor contains field "duree"
+    public Reservation(int reservationId, @NotNull Time horaire, @NotNull Bureau bureau, @NotNull Client client, int duree) {
+        this.reservationId = reservationId;
+        this.horaire = horaire;
+        this.bureau = bureau;
+        this.client = client;
+        this.duree = duree;
+    }
+    //this constructor works out "duree" from two instants instantDebut & instantFin
+    public Reservation(int reservationId, @NotNull Time horaire, @NotNull Bureau bureau, @NotNull Client client, Instant instantDebut, Instant instantFin) {
+        this.reservationId = reservationId;
+        this.horaire = horaire;
+        this.bureau = bureau;
+        this.client = client;
+        this.duree = (int) Duration.between(instantDebut, instantFin).getSeconds();
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Time getHoraire() {
+        return horaire;
+    }
+
+    public void setHoraire(Time horaire) {
+        this.horaire = horaire;
+    }
+
+    public Bureau getBureau() {
+        return bureau;
+    }
+
+    public void setBureau(Bureau bureau) {
+        this.bureau = bureau;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public int getDuree() { return duree; }
+
+    public void setDuree(int duree) { this.duree = duree; }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "reservationId=" + reservationId +
+                ", horaire=" + horaire +
+                ", bureau=" + bureau +
+                ", client=" + client +
+                ", duree=" + duree +
+                '}';
+    }
+}

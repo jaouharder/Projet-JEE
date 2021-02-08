64678d9ebcb099f580bab277e0a09ed576172792
T470s <benettalebamine@gmail.com>
2021-02-08 19:24:01 +0100

    Client

--- ReservationSystem/src/main/java/com/ReservationSystem/model/Client.java
+++ ReservationSystem/src/main/java/com/ReservationSystem/model/Client.java
@@ -17,43 +17,43 @@ public class Client {
 			this.email = email;
 		}
 	//constructor without localisation
-				public Client(String cin, String nom, String prenom, String email) {
-					super();
-					this.cin = cin;
-					this.nom = nom;
-					this.prenom = prenom;
-					this.email = email;
-				}
+			public Client(String cin, String nom, String prenom, String email) {
+				super();
+				this.cin = cin;
+				this.nom = nom;
+				this.prenom = prenom;
+				this.email = email;
+			}
+	//getters & setters			
+			public String getCin() {
+				return cin;
+			}
+			public void setCin(String cin) {
+				this.cin = cin;
+			}
+			public String getNom() {
+				return nom;
+			}
+			public void setNom(String nom) {
+				this.nom = nom;
+			}
+			public String getPrenom() {
+				return prenom;
+			}
+			public void setPrenom(String prenom) {
+				this.prenom = prenom;
+			}
+			public String getLocalisation() {
+				return localisation;
+			}
+			public void setLocalisation(String localisation) {
+				this.localisation = localisation;
+			}
+			public String getEmail() {
+				return email;
+			}
+			public void setEmail(String email) {
+				this.email = email;
+			}
 
-	//getters & setters
-		public String getCin() {
-			return cin;
-		}
-		public void setCin(String cin) {
-			this.cin = cin;
-		}
-		public String getNom() {
-			return nom;
-		}
-		public void setNom(String nom) {
-			this.nom = nom;
-		}
-		public String getPrenom() {
-			return prenom;
-		}
-		public void setPrenom(String prenom) {
-			this.prenom = prenom;
-		}
-		public String getLocalisation() {
-			return localisation;
-		}
-		public void setLocalisation(String localisation) {
-			this.localisation = localisation;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
 }

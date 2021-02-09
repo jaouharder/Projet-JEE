898b29e6e493bcfdb4d3e081926d50ada148d3c9
jaouharder <jaouharder@gmail.com>
2021-02-09 12:34:36 +0100

    Mth getBureauxByAvb added

--- ReservationSystem/src/main/java/com/ReservationSystem/dao/BureauDAO.java
+++ ReservationSystem/src/main/java/com/ReservationSystem/dao/BureauDAO.java
@@ -8,6 +8,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,58 +19,79 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ReservationSystem.configdb.JDBCONFIG;
 import com.ReservationSystem.model.Agence;
 import com.ReservationSystem.model.Bureau;
+import com.ReservationSystem.model.Reservation;
 
 @SpringBootApplication
 public class BureauDAO {
 	static Connection conx=JDBCONFIG.GetConx();
 	
 	
-	public static void getBureaux(int agenceId) {
-		//SpringApplication.run(BureauDAO.class, args);	
-		
-		/*
-		 * try {
-		 * 
-		 * Statement stat=conx.createStatement(); ResultSet
-		 * resultagc=stat.executeQuery("select * from agence where agence_Id="+agenceId+
-		 * ";");
-		 * 
-		 * if (!resultagc.next()) { System.out.println(" INVALID");} else { String
-		 * agname=resultagc.getString("agence_name"); String
-		 * localisation=resultagc.getString("localisation");
-		 * System.out.println("L'agence  : "+agname+"\nlocalisation : "
-		 * +localisation+"\nIl y a les sevices suivant: "); ResultSet result=
-		 * stat.executeQuery("select * from bureau where agence_Id="+agenceId+";");
-		 * while(result.next()) { String srv=result.getString("service"); int
-		 * disponibilite=result.getInt("bureau_disponibilite");
-		 * System.out.println("Service : "+srv+"   -----   disponibilte : "
-		 * +disponibilite+" %"); } } }catch (SQLException e) {e.printStackTrace();}
-		 */	
+	public static List<Bureau> getBureauxByAvb(String service,String city) {		
+		  try {
+		  
+		  Statement stat=conx.createStatement(); 
+		  List<Bureau> bureaux= new ArrayList<Bureau>();
+		  String query="SELECT * FROM agence AS ag JOIN bureau AS br ON ag.agence_Id=br.agence_Id WHERE ag.city='"+city+"' AND br.isavailable=1 AND br.service='"+service+"';";
+		  ResultSet resultagc=stat.executeQuery(query);		  
+		//  if (!resultagc.next()) { System.err.println("NO AGENCE AVAILABLE IN THIS CITY WITH THIS SERVICE ");} 
+		 // else {
+		  while(resultagc.next()) {
+			      int avb=resultagc.getInt("bureau_availability");
+			      String agname=resultagc.getString("agence_name");
+			      String local=resultagc.getString("localisation");
+			      Agence agc= new Agence(agname, local);
+				  Bureau br = new Bureau(service,agc,avb);
+				  bureaux.add(br);
+			     } 
+		  System.out.println(bureaux.toString());
+			     Collections.sort(bureaux,new Comparator<Bureau>() {
+
+					@Override
+					public int compare(Bureau o1, Bureau o2) {
+						int ds2=o2.getBureau_disp(),ds1=o1.getBureau_disp();
+						if(ds1>ds2) return 1;
+						else if (ds1==ds2) return 0;
+						else return -1;
+					}
+				});
+		  return bureaux;  
+		  //  }
+		  }catch (SQLException e) {e.printStackTrace();}
+		return null;
+		 	
 	}
 public static void main(String[] args) throws SQLException {
 	SpringApplication.run(BureauDAO.class, args);
-	getBureaux(8);
-	//findById(1);
+	System.out.println(getBureauxByAvb("CREATION DE COMPTE","meknes").toString());
+	//getBureaux(8);
+//	System.out.println(findById(2).toString());
 	}
-public static Bureau findById(int bureauId) throws SQLException {
+public  Bureau findById(int bureauId) throws SQLException {
 	
 	Statement stat=conx.createStatement();
 	ResultSet resultagc=stat.executeQuery("select * from bureau where bureau_Id="+bureauId+";");
-	resultagc.next();
+	if(!resultagc.next()) {
+		System.err.println("NO BUREAU WITH ID = "+bureauId);
+		return null;
+	}
+	else {
 	int id = resultagc.getInt("bureau_Id");
 	String service=resultagc.getString("service");
-	int dispo=resultagc.getInt("bureau_disponibilite");
-	Agence agc= AgenceDAO.findById(resultagc.getInt("agence_Id")); 
-	Bureau br = new Bureau(id,service,dispo,agc);
+	int dispo=resultagc.getInt("bureau_availability");
+	boolean disp = resultagc.getBoolean("isavailable");
+	AgenceDAO agcDao = new AgenceDAO();
+	Agence agc= agcDao.findById(resultagc.getInt("agence_Id")); 
+	Bureau br = new Bureau(id,service,dispo,agc, disp);
 	return br;
+	}
 }
-public static String authentification(String username,String password,int bureauId) throws SQLException {
+public  String authentification(String username,String password,int bureauId) throws SQLException {
 	Statement stat=conx.createStatement();
 	ResultSet result=stat.executeQuery("select * from employee where username="+username+";");
 	
 	if(!result.next()) return "INVALID USERNAME";
 	else if(password.equals(result.getString("password"))) {
-		int Rows=stat.executeUpdate("UPDATE employee SET bureau_Id="+bureauId+" WHERE username="+username+";");
+		stat.executeUpdate("UPDATE employee SET bureau_Id="+bureauId+" WHERE username="+username+";");
 		return "ACCESS GAINED";
 	}
 	    else {

1d8183a44b859d5766e92f2ebcb87f40c873485c
Derrouich Jaouhar <58748867+jaouharder@users.noreply.github.com>
2021-03-01 01:54:55 +0100

    Add files via upload

--- FRONT PROJET/EditReservation/NEW/edit_reservation.js
+++ FRONT PROJET/EditReservation/NEW/edit_reservation.js
@@ -1,4 +1,4 @@
-async function GetOldReservation(){
+/* async function GetOldReservation(){
 
     // const data = await fetch('http://localhost:8080/reservation/6');
     // let old_reservation = await data.json();
@@ -17,6 +17,8 @@ async function GetOldReservation(){
     submitbtn.addEventListener('click', submit_mod)
 }
 GetOldReservation();
+
+
 
 function submit_mod() {
 
@@ -42,3 +44,41 @@ function submit_mod() {
 
 }
 
+ */
+async function GetOldReservation(){
+
+    // const data = await fetch('http://localhost:8080/reservation/6');
+    // let old_reservation = await data.json();
+
+    let old_reservation = JSON.parse(sessionStorage.getItem("oldreservation"));
+    // let old_reservation = sessionStorage.getItem("oldreservation");
+    // console.log(old_reservation);
+
+    document.getElementById("cin").value = old_reservation.client.cin;
+    document.getElementById("first_name").value = old_reservation.client.prenom;
+    document.getElementById("last_name").value = old_reservation.client.nom;
+    document.getElementById("email").value = old_reservation.client.email;
+    document.getElementById("agency_name").value = old_reservation.bureau.agence.nom;
+    sessionStorage.setItem("tempoReserv", JSON.stringify(old_reservation));
+    let submitbtn = document.getElementById("change");
+    submitbtn.addEventListener('click', submit_mod)
+}
+GetOldReservation();
+
+function submit_mod() {
+
+    let new_reservation = JSON.parse(sessionStorage.getItem("tempoReserv"));
+
+
+    let service = document.getElementById("service");
+   // sessionStorage.setItem("service", service.value);
+     let agence = new_reservation.bureau.agence;
+    new_reservation.bureau = new_reservation.bureau.agence.bureauList[service.value];
+    new_reservation.bureau.agence = agence;
+    sessionStorage.setItem("service",new_reservation.bureau.bureauId);
+    sessionStorage.setItem("operation", "modifier");
+    window.location.href='../Calendar/fullcalendar.html';
+
+    // console.log(new_reservation);
+
+}
